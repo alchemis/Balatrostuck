26643f8bdfaf75880537f3db577cf717babf50aa
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-01-04 13:18:04 -0600

    bugfix

--- src/data/jokers/j_vasterror.lua
+++ src/data/jokers/j_vasterror.lua
@@ -35,12 +35,10 @@ function Balatrostuck.INIT.Jokers.j_vasterror()
 
         calculate = function(self, card, context)
             if context.reroll_shop and not context.blueprint then
-
+                card.ability.extra.rolls = card.ability.extra.rolls + 1
                 if card.ability.extra.rolls >= card.ability.extra.rolls_needed then
                     card_eval_status_text(card, 'extra', nil, nil, nil, {message = 'Paradox-ify!',colour = G.C.GREEN, card = card})
-                    if not context.blueprint then
-                        card.ability.extra.rolls = 0
-                    end
+                    card.ability.extra.rolls = 0
                     local _card = pseudorandom_element(G.deck.cards,pseudoseed("arcjetted"))
                     _card:set_edition("e_bstuck_paradox",true)
                     _card:set_seal("Blue")
