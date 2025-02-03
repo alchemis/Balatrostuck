34920dca1a1e6e4fdbc370e9a0e62cbfb70a055d
a-memer <110717705+a-memer@users.noreply.github.com>
2025-02-02 18:42:51 -0600

    Added Void

--- src/data/aspects/c_aspect_void.lua
+++ src/data/aspects/c_aspect_void.lua
@@ -27,6 +27,42 @@ function Balatrostuck.INIT.Aspects.c_aspect_void()
                     summation(1+(G.GAME.BALATROSTUCK.aspect_levels[self.name] or 0)),
                 }
             }
+        end,
+        use = function(self, context)
+            self:switch_slab()
+        end,
+        can_use = function(self)
+            return true
+        end
+    }
+
+    Balatrostuck.Slab{
+        key = 'void',
+        atlas = 'HomestuckAspectSlabs',
+        pos = {
+            x = 1,
+            y = 1
+        },
+        config = {},
+        name = 'Aspect of Void',
+        apply = function(self, slab, context) 
+            if context.setting_blind then
+                local negativeCount = 0
+                local jokersToChoose = {}
+                for j = 1, #G.jokers.cards do
+                    if G.jokers.cards[j].edition then
+                        if G.jokers.cards[j].edition.negative then negativeCount = negativeCount + 1 end
+                    else
+                        table.insert(jokersToChoose, G.jokers.cards[j])
+                    end
+                end
+
+                if #jokersToChoose > 0 and negativeCount < summation(slab:level()) then
+                    local flintstones = pseudorandom_element(jokersToChoose, pseudoseed('void'))
+                    flintstones:set_edition({negative = true}, true)
+                    flintstones.temp_edition = nil
+                end
+            end
         end
     }
 end
