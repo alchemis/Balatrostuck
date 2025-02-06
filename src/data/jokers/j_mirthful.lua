7bf44a57b02466971630cdb8590f064c4c413c3c
a-memer <110717705+a-memer@users.noreply.github.com>
2025-02-06 05:25:45 -0600

    Added the Mirthful Joker

--- src/data/jokers/j_mirthful.lua
+++ src/data/jokers/j_mirthful.lua
@@ -4,17 +4,19 @@ function Balatrostuck.INIT.Jokers.j_mirthful()
         name = "Mirthful Joker",
         key = "mirthful",
         config = {
-            extra = {
-            }
+            extra = {num_odds = 69, dom_odds = 420}
         },
         loc_txt = {
             ['name'] = 'Mirthful Joker',
             ['text'] = {
-                [1] = "{C:green}69 in 420{} chance to",
+                [1] = "{C:green}#1# in #2#{} chance to",
                 [2] = "create a {C:attention}Zodiac card{}",
                 [3] = "at end of round"
             }
         },
+        loc_vars = function(self,info_queue,card)
+            return {vars = {card.ability.extra.num_odds * G.GAME.probabilities.normal, card.ability.extra.dom_odds }}
+        end,
         pos = {
             x = 0,
             y = 5
@@ -25,8 +27,33 @@ function Balatrostuck.INIT.Jokers.j_mirthful()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
-        
+        atlas = 'HomestuckJokers',
+        calculate = function(self,card,context)
+            if context.end_of_round and context.cardarea == G.jokers then
+                if pseudorandom('Mirthful') < (card.ability.extra.num_odds * G.GAME.probabilities.normal) / card.ability.extra.dom_odds and
+                #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                    G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                    return {
+                        extra = {focus = card, message = '+1 Zodiac!', func = function()
+                            G.E_MANAGER:add_event(Event({
+                                trigger = 'before',
+                                delay = 0.0,
+                                func = (function()
+                                        local zodiac = SMODS.create_card({set = 'Zodiac'})
+                                        zodiac:add_to_deck()
+                                        G.consumeables:emplace(zodiac)
+                                        G.GAME.consumeable_buffer = 0
+                                    return true
+                                end)}))
+                        end},
+                        colour = G.C.SECONDARY_SET.Tarot,
+                        card = card
+                    }
+                end
+            end
+        end
+
+        -- PRESERVED FOR SAFE KEEPING :) - Delirium
         -- SMODS.Jokers.j_mirthful.calculate_joker = function (self, context)
         --     if context.cardarea == G.jokers and next(context.poker_hands["Straight"]) and not (context.individual or context.repetition or context.before or context.after) then
         --         local has_6 = false
