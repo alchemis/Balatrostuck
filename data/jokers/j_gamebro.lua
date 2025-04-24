1e4f03a1ab3d18718165ccf0d12ce8945ca43cd7
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-04-24 12:24:47 -0600

    gamebro programmed

--- data/jokers/j_gamebro.lua
+++ data/jokers/j_gamebro.lua
@@ -29,11 +29,32 @@ function Balatrostuck.INIT.Jokers.j_gamebro()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
+        enhancement_gate = 'm_bonus',
         atlas = 'HomestuckJokers',
 
         loc_vars = function (self, info_queue, card)
             info_queue[#info_queue+1] = G.P_CENTERS.m_bonus 
             return {vars = {card.ability.extra.odds, card.ability.extra.odds_divisor, card.ability.extra.mult, card.ability.extra.mult_increase, card.ability.extra.odds_increase}}
         end,
+
+        calculate = function(self, card, context)                
+            if context.joker_main and context.cardarea == G.jokers then
+                for k, v in ipairs(context.scoring_hand) do
+                    if SMODS.get_enhancements(v).m_bonus == true then
+                        card.ability.extra.odds = card.ability.extra.odds + card.ability.extra.odds_increase
+                        card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_increase
+                    end
+                end
+                if pseudorandom("tokeepitreal") < card.ability.extra.odds / card.ability.extra.odds_divisor then
+                    return{
+                        mult_mod = card.ability.extra.mult,
+                        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+                    }
+                end
+            elseif context.after then 
+                card.ability.extra.odds = 0
+                card.ability.extra.mult = 0
+            end
+        end
     }
 end 
