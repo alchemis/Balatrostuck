4401a24a15ca66ffc5c2f800d0603baf063a4dfe
Gainumki <gabrytechgamer@gmail.com>
2025-04-25 20:54:31 +0200

    Porkhollow

--- data/jokers/j_porkhollow.lua
+++ data/jokers/j_porkhollow.lua
@@ -4,13 +4,18 @@ function Balatrostuck.INIT.Jokers.j_porkhollow()
         key = "porkhollow",
         config = {
             extra = {
+                starting_chips = 15,
+                chips = 15,
+                chips_gain = 3
             }
         },
         loc_txt = {
             ['name'] = 'Porkhollow',
             ['text'] = {
-                [1] = "Gain no interest; instead, that money",
-                [2] = "goes to this joker's sell value. +3 chips for each $ in sell value. Currently: +15 chips"
+                [1] = "Gain no interest", 
+                [2] = "Instead, that money goes to this joker's sell value.",
+                [3] = "{C:chips}+#2#{} chips for each {C:money}${} in sell value.", 
+                [4] = "{C:inactive}(Currently: {C:chips}+#1#{}{C:inactive} Chips)"
             }
         },
         pos = {
@@ -23,6 +28,47 @@ function Balatrostuck.INIT.Jokers.j_porkhollow()
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
+        atlas = 'HomestuckJokers',
+
+        loc_vars = function(self, info_queue, card)
+            return { vars = { card.ability.extra.chips, card.ability.extra.chips_gain } }
+          end,        
+
+        add_to_deck = function(self, card)
+            if not G.GAME.selected_back.effect.config.no_interest then
+                G.GAME.modifiers.no_interest = true
+            end
+        end,
+
+        remove_from_deck = function(self, card)
+            if not G.GAME.selected_back.effect.config.no_interest then
+                G.GAME.modifiers.no_interest = false
+            end
+        end,
+        
+        calculate = function(self,card,context)
+            if context.end_of_round and context.main_eval and not context.blueprint then
+                card.ability.extra.mult_gain = 0
+                if G.GAME.modifiers.no_interest then
+                    card.ability.extra_value = card.ability.extra_value + G.GAME.interest_amount*math.min(math.floor(G.GAME.dollars/5), G.GAME.interest_cap/5)
+                    card:set_cost()
+                end
+                local cur_chips = card.ability.extra.chips
+                card.ability.extra.chips = card.ability.extra.starting_chips + card.ability.extra.chips_gain * card.ability.extra_value
+                if card.ability.extra.chips ~= cur_chips then
+                    return {
+                        card = card,
+                        message = "Upgraded!"
+                    }
+                end
+            end
+
+            if context.joker_main then
+                return {
+                  chip_mod = card.ability.extra.chips,
+                  message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
+                }
+              end          
+        end
     }
 end 
