e9d8093aef646bfca188d5b0c64c904bbdb97ce0
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-04-30 10:50:03 -0600

    progammed CotL

--- data/jokers/j_complacencyofthelearned.lua
+++ data/jokers/j_complacencyofthelearned.lua
@@ -4,13 +4,16 @@ function Balatrostuck.INIT.Jokers.j_complacencyofthelearned()
         key = "complacencyofthelearned",
         config = {
             extra = {
+                x_mult = 6,
+                scored = 0,
+                triggers = 0
             }
         },
         loc_txt = {
             ['name'] = 'Complacency of the Learned',
             ['text'] = {
                 [1] = "If a {C:attention}Lucky Card {C:green}succeeds{},",
-                [2] = "{X:red,C:white}XMult{} equal to {C:attention}6{} multiplied",
+                [2] = "{X:red,C:white}XMult{} equal to {C:attention}#1#{} multiplied",
                 [3] = "by the number of times Lucky",
                 [4] = "cards {C:attention}succeeded{}, divided by how",
                 [5] = "many times cards were {C:attention}scored"
@@ -20,12 +23,36 @@ function Balatrostuck.INIT.Jokers.j_complacencyofthelearned()
             x = 2,
             y = 0
          },
-        cost = 8,
+        cost = 6,
         rarity = 2,
         blueprint_compat = true,
         eternal_compat = true,
         unlocked = true,
         discovered = true,
-        atlas = 'HomestuckJokers'
+        atlas = 'HomestuckJokers',
+        loc_vars = function(self,info_queue,card)
+            return {
+                vars = {card.ability.extra.x_mult}
+            }
+        end,
+
+        calculate = function(self, card, context)
+            if context.before and not context.blueprint then 
+                card.ability.extra.scored = 0
+                card.ability.extra.triggers = 0
+            end
+            if context.individual and context.cardarea == G.play and not context.blueprint then
+                card.ability.extra.scored = card.ability.extra.scored + 1
+                if context.other_card.lucky_trigger then
+                    card.ability.extra.triggers = card.ability.extra.triggers + 1
+                end
+            end
+            if card.ability.extra.triggers > 0 and context.cardarea == G.jokers and context.joker_main then
+                return {
+                    x_mult = card.ability.extra.x_mult * (card.ability.extra.triggers / card.ability.extra.scored),
+                    card = card
+                }
+            end
+        end
     }
 end 
