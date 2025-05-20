defbb0023006deb81ff2457571922a7025e400ae
akaigaming <vriskatfs@proton.me>
2025-05-20 23:56:25 +0100

    More robust quip system

--- pg_events.lua
+++ pg_events.lua
@@ -12,37 +12,49 @@ end
 
 PG.John.shop_events = {
     free_voucher = {
-        quip = 'john_free_voucher',
+        quip = {
+            {ref='john_free_voucher'}
+        },
         func = function(_self,card,context)
           
         end
     },
     discount_card = {
-        quip = 'john_discount_card',
+        quip = {
+            {ref='john_discount_card'}
+        },
         func = function (_self,card,context)
             
         end
     },
     add_pack = {
-        quip = 'john_add_pack',
+        quip = {
+            {ref='john_add_pack'}
+        },
         func = function (_self,card,context)
             
         end
     },
     free_reroll = {
-        quip = 'john_free_reroll',
+        quip = {
+            {ref='john_free_reroll'}
+        },
         func = function (_self,card,context)
             
         end
     },
     add_rare = {
-        quip = 'john_add_rare',
+        quip = {
+            {ref='john_add_rare'}
+        },
         func = function (_self,card,context)
             
         end
     },
     add_uncommon = {
-        quip = 'john_add_uncommon',
+        quip = {
+            {ref='john_add_uncommon'}
+        },
         func = function (_self,card,context)
             
         end
@@ -51,19 +63,25 @@ PG.John.shop_events = {
 
 PG.John.start_of_round_events = {
     paradox_common = {
-        quip = 'john_paradox_common',
+        quip = {
+            {ref='john_paradox_common'}
+        },
         func = function (_self,card,context)
             
         end
     },
     paradox_playingcard = {
-        quip = 'john_paradox_playingcard',
+        quip = {
+            {ref='john_paradox_playingcard'}
+        },
         func = function (_self,card,context)
         end
     },
     draw = {
         size = 2,
-        quip = 'john_draw',
+        quip = {
+            {ref='john_draw'}
+        },
         func = function (self,_self,card,context)
             
         end
@@ -72,25 +90,44 @@ PG.John.start_of_round_events = {
 
 PG.John.before_scoring_events = {
     add_playing_edition = {
-        quip = 'john_add_playing_edition',
+        quip = {
+            {ref='john_add_playing_edition'}
+        },
         func = function (_self, card, context)
 
         end
     },
     level_poker_hand = {
-        quip = 'john_level_poker_hand',
+        quip = {
+            {ref='john_level_poker_hand'}
+        },
         func = function (_self, card, context)
             
         end
     },
     add_paradox_steel_hand = {
-        quip = 'john_add_paradox_steel_hand',
+        quip = {
+            {ref='john_add_paradox_steel_hand'}
+        },
         func = function (_self, card, context)
 
         end
     },
     cheat_hand_type = {
-        quip = 'john_cheat_hand_type',
+        quip = {
+            {ref='john_cheat_hand_type'}
+        },
+        func = function (_self, card, context)
+            
+        end
+    }
+}
+
+PG.Caliborn.start_of_round_events = {
+    reset_deck = {
+        quip = {
+            {ref='caliborn_reset_deck1', time=0.4, pause=0.2}
+        },
         func = function (_self, card, context)
             
         end
