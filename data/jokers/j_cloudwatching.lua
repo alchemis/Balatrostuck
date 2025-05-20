d3fae514067010a21afa2be33b046adca55d9877
a-memer <110717705+a-memer@users.noreply.github.com>
2025-05-20 11:09:00 -0500

    Fixed cloudwatching creating permenetly debuffed cards

--- data/jokers/j_cloudwatching.lua
+++ data/jokers/j_cloudwatching.lua
@@ -27,22 +27,22 @@ function Balatrostuck.INIT.Jokers.j_cloudwatching()
         calculate = function (self, card, context)
             if context.first_hand_drawn then
                 local firstmat = nil
-                for i=1, 1 do
+                if #G.deck.cards >= 15 then
+                    G.playing_card = (G.playing_card and G.playing_card + 1) or 1
+                    local _card = copy_card(G.deck.cards[#G.deck.cards-14], nil, nil, G.playing_card)
+                    _card:add_to_deck()
+                    _card:set_edition('e_bstuck_paradox')
+                    G.deck.config.card_limit = G.deck.config.card_limit + 1
+                    table.insert(G.playing_cards, _card)
+                    G.hand:emplace(_card)
+                    _card.states.visible = nil
+
                     G.E_MANAGER:add_event(Event({
-                        func = function() 
-                            G.playing_card = (G.playing_card and G.playing_card + 1) or 1
-                            local _card = copy_card(G.deck.cards[#G.deck.cards - 14])
-                            _card:start_materialize({G.C.PARADOX},firstmat)
-                            firstmat = true
-                            _card:set_edition('e_bstuck_paradox',true,true)
-                            G.hand:emplace(_card)
-                            G.GAME.blind:debuff_card(_card)
-                            G.hand:sort()
-                            if context.blueprint_card then context.blueprint_card:juice_up() else card:juice_up() end
-                            -- G.deck.config.card_limit = G.deck.config.card_limit + 1
-                            delay(0.1)
+                        func = function()
+                            _card:start_materialize()
                             return true
-                        end}))
+                        end
+                    }))
                     playing_card_joker_effects({_card})
                 end
             end
