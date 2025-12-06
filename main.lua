8dcb4a848446489e08bdf7bd88785a7b0f9d24a2
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-12-05 21:46:03 -0700

    hiveswap act 1 unlock fix

--- main.lua
+++ main.lua
@@ -278,7 +278,7 @@ else
   UI()
 end
 
-UnlockedByApple = {"skaia", "ectobiology", "purrfectwarrior", "vodkamutini", "lordoftime", "museofspace", "lofaf", "lowas", "lolar", "lohac", "sucker", "ascend", "mobiusdoublereacharound", "frustration", "problemsleuth", "aceDick", "pickleinspector", "whatpumpkin", "pairing_grid", "consortconcierge", "roundtwo", "tumor", "innapropriatebucket", "genejoker", "kernelsprite", "trolltrain", "misspaint", "culling", "smallerbutterflies", "hotdogjuggler", "waywardvagabond", "amberfirefly", "whitequeen", "parcelmistress", "aimlessrenegade"}
+UnlockedByApple = {"skaia", "ectobiology", "purrfectwarrior", "vodkamutini", "lordoftime", "museofspace", "lofaf", "lowas", "lolar", "lohac", "sucker", "ascend", "mobiusdoublereacharound", "frustration", "problemsleuth", "aceDick", "pickleinspector", "whatpumpkin", "pairing_grid", "consortconcierge", "roundtwo", "tumor", "innapropriatebucket", "genejoker", "kernelsprite", "trolltrain", "misspaint", "culling", "smallerbutterflies", "hotdogjuggler", "waywardvagabond", "amberfirefly", "whitequeen", "parcelmistress", "aimlessrenegade","balletslippers","lonegunbird","oldsecret"}
 UnlockedByAscend = {"beyondcanon", "batterwitch", "therapture", "ahabscrosshairs", "clownincar", "gristtorrent", "enterthemedium", "whoisthis"}
 UnlockedbyEnterTheMedium = {"faygo", "stump", "ringofvoid", "porkhollow", "ringoflife", "descend"}
 UnlockedByDescend = {"cascade", "wake", "complacencyofthelearned", "gamebro", "dadswallet", "darkscholar"}
